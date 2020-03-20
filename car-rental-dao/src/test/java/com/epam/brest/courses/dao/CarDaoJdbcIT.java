1252e57f0a184b3ca7d1dd9ba00fb7a9b5e87a1a
Anastasiya Piashko <anapiashko@gmail.com>
2020-03-21 00:05:42 +0300

    carDao tests

--- car-rental-dao/src/test/java/com/epam/brest/courses/dao/CarDaoJdbcIT.java
+++ car-rental-dao/src/test/java/com/epam/brest/courses/dao/CarDaoJdbcIT.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml", "classpath:dao.xml"})
@@ -28,5 +30,96 @@ class CarDaoJdbcIT {
         List<Car> cars = carDao.findAll();
         assertNotNull(cars);
         assertTrue(cars.size() > 0);
+    }
+
+    @Test
+    void findById() {
+        //given
+        Car car = new Car();
+        car.setBrand("Honda");
+        car.setRegisterNumber("5302 AB-1");
+        car.setPrice(BigDecimal.valueOf(150));
+
+        Integer id = carDao.create(car);
+
+        //when
+        Optional<Car> optionalCar = carDao.findById(id);
+
+        //then
+        assertTrue(optionalCar.isPresent());
+        assertEquals(id,optionalCar.get().getId());
+        assertEquals("Honda", optionalCar.get().getBrand());
+        assertEquals("5302 AB-1", optionalCar.get().getRegisterNumber());
+        assertEquals(0 ,BigDecimal.valueOf(150).compareTo(optionalCar.get().getPrice()));
+    }
+
+    @Test
+    void create() {
+        //given
+        Car car = new Car();
+        car.setBrand("Honda");
+        car.setRegisterNumber("5302 AB-1");
+        car.setPrice(BigDecimal.valueOf(150));
+
+        //when
+        Integer id = carDao.create(car);
+
+        //then
+        assertNotNull(id);
+
+        Optional<Car> optionalCar = carDao.findById(id);
+        assertTrue(optionalCar.isPresent());
+
+        assertEquals(id,optionalCar.get().getId());
+        assertEquals("Honda", optionalCar.get().getBrand());
+        assertEquals("5302 AB-1", optionalCar.get().getRegisterNumber());
+    }
+
+    @Test
+    void update() {
+        //given
+        Car car = new Car();
+        car.setBrand("Honda");
+        car.setRegisterNumber("5302 AB-1");
+        car.setPrice(BigDecimal.valueOf(150));
+
+        Integer id = carDao.create(car);
+        Optional<Car> optionalCar = carDao.findById(id);
+
+        assertTrue(optionalCar.isPresent());
+        optionalCar.get().setBrand("HONDA");
+        optionalCar.get().setRegisterNumber("7350 AB-1");
+        optionalCar.get().setPrice(BigDecimal.valueOf(200));
+
+        //when
+        int result = carDao.update(optionalCar.get());
+
+        //then
+        assertTrue(1 == result);
+        Optional<Car> updatedOptionalCar = carDao.findById(id);
+        assertTrue(optionalCar.isPresent());
+        assertEquals(id,updatedOptionalCar.get().getId());
+        assertEquals("HONDA", updatedOptionalCar.get().getBrand());
+        assertEquals("7350 AB-1", updatedOptionalCar.get().getRegisterNumber());
+        assertEquals(0, BigDecimal.valueOf(200).compareTo(updatedOptionalCar.get().getPrice()));
+    }
+
+    @Test
+    void delete() {
+        //given
+        Car car = new Car();
+        car.setBrand("Honda");
+        car.setRegisterNumber("5302 AB-1");
+        car.setPrice(BigDecimal.valueOf(150));
+
+        Integer id = carDao.create(car);
+
+        //when
+        int result = carDao.delete(id);
+
+        //then
+        assertTrue(1 == result);
+        Optional<Car> optionalCar = carDao.findById(id);
+        assertFalse(optionalCar.isPresent());
     }
 }
