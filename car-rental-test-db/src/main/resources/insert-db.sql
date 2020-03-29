5b78d905091e99a8d1911f539c5d9819a5cf39b7
Anastasiya Piashko <anapiashko@gmail.com>
2020-03-29 17:31:14 +0300

    send dates to controller (templates)

--- car-rental-test-db/src/main/resources/insert-db.sql
+++ car-rental-test-db/src/main/resources/insert-db.sql
@@ -6,3 +6,8 @@ INSERT INTO car (car_id, car_brand, car_price, car_register_number) VALUES (3, '
 
 INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (1, '2020-01-01', 2);
 INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (2, '2020-01-29', 3);
+INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (3, '2020-01-07', 2);
+
+INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (4, '2020-04-03', 1);
+INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (5, '2020-04-03', 3);
+INSERT INTO order_record(order_record_id, order_date, car_id) VALUES (6, '2020-04-03', 2);
