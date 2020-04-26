213c0ebccee586bbea7073e289905838cfca0bff
Anastasiya Piashko <anapiashko@gmail.com>
2020-04-26 18:38:11 +0300

    add service and service-api modules

---
+++ car-rental-service/src/main/java/com/epam/brest/courses/service/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package com.epam.brest.courses.service;
+
+import com.epam.brest.courses.dao.OrderDao;
+import com.epam.brest.courses.model.Order;
+import com.epam.brest.courses.service_api.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Implementation of order service interface.
+ */
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
+
+    private final OrderDao orderDao;
+
+    @Autowired
+    public OrderServiceImpl(final OrderDao orderDao) {
+        this.orderDao = orderDao;
+    }
+
+    @Override
+    public final Integer create(final Order order) {
+        LOGGER.debug("create (order:{})", order);
+
+        return orderDao.create(order);
+    }
+}
