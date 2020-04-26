b851d68658922b433edf39f408e92ae1270f66c2
Anastasiya Piashko <anapiashko@gmail.com>
2020-04-26 17:28:55 +0300

    add model module

---
+++ car-rental-model/src/main/java/com/epam/brest/courses/constants/CarConstants.java
@@ -0,0 +1,24 @@
+package com.epam.brest.courses.constants;
+
+/**
+ * Constants connected with car fields.
+ */
+public final class CarConstants {
+
+    /**
+     * CarConstants constructor.
+     */
+    private CarConstants() {
+
+    }
+
+    /**
+     * Length of car brand name.
+     */
+    public static final int CAR_BRAND_SIZE = 45;
+
+    /**
+     * Length of car registration number name.
+     */
+    public static final int CAR_REGISTER_SIZE = 15;
+}
