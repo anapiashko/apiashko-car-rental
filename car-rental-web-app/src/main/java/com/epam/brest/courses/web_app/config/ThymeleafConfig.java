cb330bf32e795ded0b9e2fd4fe641b2d8adb1c48
Anastasiya Piashko <anapiashko@gmail.com>
2020-05-03 03:20:36 +0300

    web config

---
+++ car-rental-web-app/src/main/java/com/epam/brest/courses/web_app/config/ThymeleafConfig.java
@@ -0,0 +1,55 @@
+package com.epam.brest.courses.web_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+@ComponentScan(basePackages = {"com.epam.brest.courses.*"})
+public class ThymeleafConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5")
+    public ClassLoaderTemplateResolver templateResolver() {
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template engine with Spring integration")
+    public SpringTemplateEngine templateEngine() {
+
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    @Description("Thymeleaf view resolver")
+    public ViewResolver viewResolver() {
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+
+}
---
+++ car-rental-web-app/src/main/java/com/epam/brest/courses/web_app/config/WebConfig.java
@@ -0,0 +1,55 @@
+package com.epam.brest.courses.web_app.config;
+
+import com.epam.brest.courses.service_rest.CarDtoServiceRest;
+import com.epam.brest.courses.service_rest.CarServiceRest;
+import com.epam.brest.courses.service_rest.OrderServiceRest;
+import com.epam.brest.courses.web_app.CarController;
+import com.epam.brest.courses.web_app.OrderController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+@Configuration
+@ComponentScan(basePackages = {"com.epam.brest.courses.*"})
+@RequiredArgsConstructor
+public class WebConfig {
+
+    private final ServiceProperties props;
+
+    @Bean
+    public OrderController orderController(){
+        return new OrderController(orderServiceRest());
+    }
+
+    @Bean
+    public CarController carController(){
+        return new CarController(carServiceRest(), carDtoServiceRest());
+    }
+
+    @Bean
+    public OrderServiceRest orderServiceRest() {
+        return new OrderServiceRest(props.getBaseUrl() + "/" + props.getOrderUrl(), restTemplate());
+    }
+
+    @Bean
+    public CarServiceRest carServiceRest() {
+        return new CarServiceRest(props.getBaseUrl() + "/" + props.getCarUrl(), restTemplate());
+    }
+
+    @Bean
+    public CarDtoServiceRest carDtoServiceRest() {
+        return new CarDtoServiceRest(props.getBaseUrl() + "/" + props.getCarDtoUrl(), restTemplate());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(Collections.singletonList(new MappingJackson2HttpMessageConverter()));
+        return restTemplate;
+    }
+}
