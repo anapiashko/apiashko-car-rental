8a0a4be7a9fcc298071e75fe671bf10310d27bfa
Anastasiya Piashko <anapiashko@gmail.com>
2020-06-22 19:40:55 +0300

    add maven profiles

---
+++ spring-boot-soap-rest/src/main/java/com/epam/brest/courses/ApplicationSoapRest.java
@@ -0,0 +1,12 @@
+package com.epam.brest.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSoapRest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSoapRest.class, args);
+	}
+}
