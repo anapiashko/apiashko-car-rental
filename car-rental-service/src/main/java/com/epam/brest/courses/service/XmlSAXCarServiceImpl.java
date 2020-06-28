a20e963d13a0cd91f3a56cfce33c1dd7dd8bba32
Anastasiya Piashko <anapiashko@gmail.com>
2020-06-28 17:39:59 +0300

    import xml archive SAXCarService

--- car-rental-service/src/main/java/com/epam/brest/courses/service/XmlSAXCarServiceImpl.java
+++ car-rental-service/src/main/java/com/epam/brest/courses/service/XmlSAXCarServiceImpl.java
@@ -4,10 +4,19 @@ import com.epam.brest.courses.model.Car;
 import com.epam.brest.courses.service_api.CarService;
 import com.epam.brest.courses.service_api.XmlService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
@@ -15,14 +24,17 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-
 @Service
+@Transactional
 public class XmlSAXCarServiceImpl implements XmlService<Car> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(XmlSAXCarServiceImpl.class);
 
     private final CarService carService;
 
@@ -33,6 +45,7 @@ public class XmlSAXCarServiceImpl implements XmlService<Car> {
 
     @Override
     public ByteArrayInputStream entitiesToXml(List<Car> objects) throws IOException {
+        LOGGER.debug("write entities from db in xml ()");
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
@@ -81,7 +94,61 @@ public class XmlSAXCarServiceImpl implements XmlService<Car> {
 
     @Override
     public void xmlToEntities(MultipartFile file) {
+        LOGGER.debug("save entities in db from xml ()");
 
+        carService.deleteAll();
+        try {
+            byte[] bytes = unarchiveFile(file);
+
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+
+            // Здесь мы определили анонимный класс, расширяющий класс DefaultHandler
+            DefaultHandler handler = new DefaultHandler() {
+                // Поле для указания, что тэг NAME начался
+                boolean tagCar = false;
+
+                // Метод вызывается когда SAXParser "натыкается" на начало тэга
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) {
+                    // Если тэг имеет имя car, то мы этот момент отмечаем - начался тэг car
+                    if (qName.equalsIgnoreCase("car")) {
+                        Car car = new Car();
+
+                        // Id attribute
+                        String id = attributes.getValue("id");
+                        car.setId(Integer.parseInt(id));
+
+                        // Brand attribute
+                        car.setBrand(attributes.getValue("brand"));
+
+                        // Registration number attribute
+                        car.setRegisterNumber(attributes.getValue("register_number"));
+
+                        // Price attribute
+                        car.setPrice(new BigDecimal(attributes.getValue("price")));
+
+                        tagCar = true;
+
+                        carService.create(car);
+                        System.out.println(car);
+                    }
+                }
+
+                @Override
+                public void endElement(String uri, String localName, String qName) {
+                    if (qName.equalsIgnoreCase("car")) {
+                        System.out.println("End Element :" + qName);
+                    }
+                }
+            };
+
+            // Стартуем разбор методом parse, которому передаем наследника от DefaultHandler, который будет вызываться в нужные моменты
+            saxParser.parse(new ByteArrayInputStream(bytes), handler);
+
+        } catch (SAXException | ParserConfigurationException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private byte[] unarchiveFile(MultipartFile file) {
